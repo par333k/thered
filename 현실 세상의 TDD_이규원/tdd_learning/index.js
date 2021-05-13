0fc10c83ef8731ce5d346b0addc90eb639370eec
par333k <par333k@naver.com>
2021-05-13 19:06:51 +0900

    테스트 우선 개발 1/2

--- 현실 세상의 TDD_이규원/tdd_learning/index.js
+++ 현실 세상의 TDD_이규원/tdd_learning/index.js
@@ -1,5 +1,12 @@
 function refineText(s) {
-    return s.replace("   ", " ").replace("  ", " ");
+    return s.replace("   ", " ")
+        .replace("\t", " ")
+        .replace("  ", " ")
+        .replace("  ", " ")
+        .replace("  ", " ")
+        .replace("\t ", " ")
+        .replace("mockist", "*******")
+        .replace("purist", "******");
 }
 
 module.exports = refineText;
--- 현실 세상의 TDD_이규원/tdd_learning/index.test.js
+++ 현실 세상의 TDD_이규원/tdd_learning/index.test.js
@@ -35,3 +35,22 @@ test.each`
     const actual = sut(source);
     expect(actual).toBe(expected);
 });
+
+test.each`
+    source | expected
+    ${"hello\t world"} | ${"hello world"}
+    ${"hello \tworld"} | ${"hello world"}
+`('sut transforms "${source}" that contains tab character to "${expected}"', ({ source, expected }) => {
+    const actual = sut(source);
+    expect(actual).toBe(expected);
+})
+
+test.each`
+    source | bannedWords | expected
+    ${"hello mockist"} | ${["mockist", "purist"]} | ${"hello *******"}
+    ${"hello purist"} | ${["mockist", "purist"]} | ${"hello ******"}
+`('sut transforms "${source}" to "${expected}"',
+    ({ source, bannedWords, expected }) => {
+        const actual = sut(source, { bannedWords });
+        expect(actual).toBe(expected);
+});
